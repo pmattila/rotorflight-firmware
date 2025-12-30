0b0b0ab2ca041ecfad9ec8a143d4382932e127df
Petri Mattila <petri.t.j.mattila@gmail.com>
2025-12-30 18:48:10 +0000

    Refactor printf support

--- src/main/target/common_post.h
+++ src/main/target/common_post.h
@@ -407,3 +407,28 @@ extern uint8_t __config_end;
 #ifndef USE_GPS
 #undef USE_GPS_PLUS_CODES
 #endif
+
+#ifdef USE_SERIAL_PRINTF
+#ifndef PRINTF_SERIAL_PORT
+#define PRINTF_SERIAL_PORT SERIAL_PORT_USART3
+#endif
+#ifndef PRINTF_SERIAL_SPEED
+#define PRINTF_SERIAL_SPEED 921600
+#endif
+#ifndef PRINTF_SERIAL_OPTIONS
+#define PRINTF_SERIAL_OPTIONS 0
+#endif
+#endif
+
+#if !defined(UNIT_TEST) && !defined(SIMULATOR_BUILD)
+#if defined(USE_NULL_PRINTF)
+#define printf(...)
+#define sprintf(...)
+#elif defined(USE_ITM_PRINTF) || defined(USE_SERIAL_PRINTF)
+#define printf(...)     tfp_printf(__VA_ARGS__)
+#define sprintf(...)    tfp_sprintf(__VA_ARGS__)
+#else
+#define printf(...)     STATIC_ASSERT(false, printf not implemented)
+#define sprintf(...)    STATIC_ASSERT(false, sprintf not implemented)
+#endif
+#endif

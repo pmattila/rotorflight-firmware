heliholic/devel

Fix printf format modifiers

src/main/io/dashboard.c
@@ -406,7 +406,7 @@ static void showGpsPage(void)
     i2c_OLED_set_line(dev, rowIndex++);
     i2c_OLED_send_string(dev, lineBuffer);
 
-    tfp_sprintf(lineBuffer, "La/Lo: %d/%d", gpsSol.llh.lat / GPS_DEGREES_DIVIDER, gpsSol.llh.lon / GPS_DEGREES_DIVIDER);
+    tfp_sprintf(lineBuffer, "La/Lo: %ld/%ld", gpsSol.llh.lat / GPS_DEGREES_DIVIDER, gpsSol.llh.lon / GPS_DEGREES_DIVIDER);
     padLineBuffer();
     i2c_OLED_set_line(dev, rowIndex++);
     i2c_OLED_send_string(dev, lineBuffer);
@@ -421,22 +421,22 @@ static void showGpsPage(void)
     i2c_OLED_set_xy(dev, HALF_SCREEN_CHARACTER_COLUMN_COUNT, rowIndex++);
     i2c_OLED_send_string(dev, lineBuffer);
 
-    tfp_sprintf(lineBuffer, "RX: %d", GPS_packetCount);
+    tfp_sprintf(lineBuffer, "RX: %ld", GPS_packetCount);
     padHalfLineBuffer();
     i2c_OLED_set_line(dev, rowIndex);
     i2c_OLED_send_string(dev, lineBuffer);
 
-    tfp_sprintf(lineBuffer, "ERRs: %d", gpsData.errors);
+    tfp_sprintf(lineBuffer, "ERRs: %ld", gpsData.errors);
     padHalfLineBuffer();
     i2c_OLED_set_xy(dev, HALF_SCREEN_CHARACTER_COLUMN_COUNT, rowIndex++);
     i2c_OLED_send_string(dev, lineBuffer);
 
-    tfp_sprintf(lineBuffer, "Dt: %d", gpsData.lastMessage - gpsData.lastLastMessage);
+    tfp_sprintf(lineBuffer, "Dt: %ld", gpsData.lastMessage - gpsData.lastLastMessage);
     padHalfLineBuffer();
     i2c_OLED_set_line(dev, rowIndex);
     i2c_OLED_send_string(dev, lineBuffer);
 
-    tfp_sprintf(lineBuffer, "TOs: %d", gpsData.timeouts);
+    tfp_sprintf(lineBuffer, "TOs: %ld", gpsData.timeouts);
     padHalfLineBuffer();
     i2c_OLED_set_xy(dev, HALF_SCREEN_CHARACTER_COLUMN_COUNT, rowIndex++);
     i2c_OLED_send_string(dev, lineBuffer);
@@ -468,7 +468,7 @@ static void showBatteryPage(void)
         int32_t amperage = getBatteryCurrent();
         // 123456789012345678901
         // Amp: DDD.D mAh: DDDDD
-        tfp_sprintf(lineBuffer, "Amp: %d.%d mAh: %d", amperage / 100, (amperage % 100) / 10, getBatteryCapacityUsed());
+        tfp_sprintf(lineBuffer, "Amp: %ld.%ld mAh: %ld", amperage / 100, (amperage % 100) / 10, getBatteryCapacityUsed());
         padLineBuffer();
         i2c_OLED_set_line(dev, rowIndex++);
         i2c_OLED_send_string(dev, lineBuffer);
@@ -604,7 +604,7 @@ static void showBBPage(void)
 static void showDebugPage(void)
 {
     for (int rowIndex = 0; rowIndex < 4; rowIndex++) {
-        tfp_sprintf(lineBuffer, "%d = %5d", rowIndex, debug[rowIndex]);
+        tfp_sprintf(lineBuffer, "%d = %5ld", rowIndex, debug[rowIndex]);
         padLineBuffer();
         i2c_OLED_set_line(dev, rowIndex + PAGE_TITLE_LINE_COUNT);
         i2c_OLED_send_string(dev, lineBuffer);

src/main/osd/osd.c
@@ -677,7 +677,7 @@ static bool osdDisplayStat(int statistic, uint8_t displayRow)
 #ifdef USE_GPS
     case OSD_STAT_MAX_SPEED:
         if (featureIsEnabled(FEATURE_GPS)) {
-            tfp_sprintf(buff, "%d%c", osdGetSpeedToSelectedUnit(stats.max_speed), osdGetSpeedToSelectedUnitSymbol());
+            tfp_sprintf(buff, "%ld%c", osdGetSpeedToSelectedUnit(stats.max_speed), osdGetSpeedToSelectedUnitSymbol());
             osdDisplayStatisticLabel(displayRow, "MAX SPEED", buff);
             return true;
         }
@@ -736,7 +736,7 @@ static bool osdDisplayStat(int statistic, uint8_t displayRow)
 
     case OSD_STAT_USED_MAH:
         if (batteryConfig()->currentMeterSource != CURRENT_METER_NONE) {
-            tfp_sprintf(buff, "%d%c", getBatteryCapacityUsed(), SYM_MAH);
+            tfp_sprintf(buff, "%ld%c", getBatteryCapacityUsed(), SYM_MAH);
             osdDisplayStatisticLabel(displayRow, "USED MAH", buff);
             return true;
         }
@@ -834,9 +834,9 @@ static bool osdDisplayStat(int statistic, uint8_t displayRow)
         #define METERS_PER_KILOMETER 1000
         #define METERS_PER_MILE      1609
         if (osdConfig()->units == UNIT_IMPERIAL) {
-            tfp_sprintf(buff, "%d%c", statsConfig()->stats_total_dist_m / METERS_PER_MILE, SYM_MILES);
+            tfp_sprintf(buff, "%ld%c", statsConfig()->stats_total_dist_m / METERS_PER_MILE, SYM_MILES);
         } else {
-            tfp_sprintf(buff, "%d%c", statsConfig()->stats_total_dist_m / METERS_PER_KILOMETER, SYM_KM);
+            tfp_sprintf(buff, "%ld%c", statsConfig()->stats_total_dist_m / METERS_PER_KILOMETER, SYM_KM);
         }
         osdDisplayStatisticLabel(displayRow, "TOTAL DISTANCE", buff);
         return true;

src/main/osd/osd_elements.c
@@ -798,7 +798,7 @@ static void osdElementCurrentDraw(osdElementParms_t *element)
 
 static void osdElementDebug(osdElementParms_t *element)
 {
-    tfp_sprintf(element->buff, "DBG %5d %5d %5d %5d", debug[0], debug[1], debug[2], debug[3]);
+    tfp_sprintf(element->buff, "DBG %5ld %5ld %5ld %5ld", debug[0], debug[1], debug[2], debug[3]);
 }
 
 static void osdElementDisarmed(osdElementParms_t *element)
@@ -829,7 +829,7 @@ static void osdBackgroundDisplayName(osdElementParms_t *element)
 static void osdElementTotalFlights(osdElementParms_t *element)
 {
     const int32_t total_flights = statsConfig()->stats_total_flights;
-    tfp_sprintf(element->buff, "#%d", total_flights);
+    tfp_sprintf(element->buff, "#%ld", total_flights);
 }
 #endif
 
@@ -1011,7 +1011,7 @@ static void osdElementGpsSats(osdElementParms_t *element)
 static void osdElementGpsSpeed(osdElementParms_t *element)
 {
     if (STATE(GPS_FIX)) {
-        tfp_sprintf(element->buff, "%c%3d%c", SYM_SPEED, osdGetSpeedToSelectedUnit(gpsConfig()->gps_use_3d_speed ? gpsSol.speed3d : gpsSol.groundSpeed), osdGetSpeedToSelectedUnitSymbol());
+        tfp_sprintf(element->buff, "%c%3ld%c", SYM_SPEED, osdGetSpeedToSelectedUnit(gpsConfig()->gps_use_3d_speed ? gpsSol.speed3d : gpsSol.groundSpeed), osdGetSpeedToSelectedUnitSymbol());
     } else {
         tfp_sprintf(element->buff, "%c%c%c", SYM_SPEED, SYM_HYPHEN, osdGetSpeedToSelectedUnitSymbol());
     }
@@ -1096,7 +1096,7 @@ static void osdElementLogStatus(osdElementParms_t *element)
         } else {
             int32_t logNumber = blackboxGetLogNumber();
             if (logNumber >= 0) {
-                tfp_sprintf(element->buff, "%c%d", SYM_BBLOG, logNumber);
+                tfp_sprintf(element->buff, "%c%ld", SYM_BBLOG, logNumber);
             } else {
                 tfp_sprintf(element->buff, "%c", SYM_BBLOG);
             }
@@ -1107,7 +1107,7 @@ static void osdElementLogStatus(osdElementParms_t *element)
 
 static void osdElementMahDrawn(osdElementParms_t *element)
 {
-    tfp_sprintf(element->buff, "%4d%c", getBatteryCapacityUsed(), SYM_MAH);
+    tfp_sprintf(element->buff, "%4ld%c", getBatteryCapacityUsed(), SYM_MAH);
 }
 
 static void osdElementMainBatteryUsage(osdElementParms_t *element)

pmattila/rotorflight-firmware

src/main/io/dashboard.c
@@ -406,7 +406,7 @@ static void showGpsPage(void)
     i2c_OLED_set_line(dev, rowIndex++);
     i2c_OLED_send_string(dev, lineBuffer);
 
-    tfp_sprintf(lineBuffer, "La/Lo: %d/%d", gpsSol.llh.lat / GPS_DEGREES_DIVIDER, gpsSol.llh.lon / GPS_DEGREES_DIVIDER);
+    tfp_sprintf(lineBuffer, "La/Lo: %ld/%ld", gpsSol.llh.lat / GPS_DEGREES_DIVIDER, gpsSol.llh.lon / GPS_DEGREES_DIVIDER);
     padLineBuffer();
     i2c_OLED_set_line(dev, rowIndex++);
     i2c_OLED_send_string(dev, lineBuffer);
@@ -421,22 +421,22 @@ static void showGpsPage(void)
     i2c_OLED_set_xy(dev, HALF_SCREEN_CHARACTER_COLUMN_COUNT, rowIndex++);
     i2c_OLED_send_string(dev, lineBuffer);
 
-    tfp_sprintf(lineBuffer, "RX: %d", GPS_packetCount);
+    tfp_sprintf(lineBuffer, "RX: %lu", GPS_packetCount);
     padHalfLineBuffer();
     i2c_OLED_set_line(dev, rowIndex);
     i2c_OLED_send_string(dev, lineBuffer);
 
-    tfp_sprintf(lineBuffer, "ERRs: %d", gpsData.errors);
+    tfp_sprintf(lineBuffer, "ERRs: %lu", gpsData.errors);
     padHalfLineBuffer();
     i2c_OLED_set_xy(dev, HALF_SCREEN_CHARACTER_COLUMN_COUNT, rowIndex++);
     i2c_OLED_send_string(dev, lineBuffer);
 
-    tfp_sprintf(lineBuffer, "Dt: %d", gpsData.lastMessage - gpsData.lastLastMessage);
+    tfp_sprintf(lineBuffer, "Dt: %lu", gpsData.lastMessage - gpsData.lastLastMessage);
     padHalfLineBuffer();
     i2c_OLED_set_line(dev, rowIndex);
     i2c_OLED_send_string(dev, lineBuffer);
 
-    tfp_sprintf(lineBuffer, "TOs: %d", gpsData.timeouts);
+    tfp_sprintf(lineBuffer, "TOs: %lu", gpsData.timeouts);
     padHalfLineBuffer();
     i2c_OLED_set_xy(dev, HALF_SCREEN_CHARACTER_COLUMN_COUNT, rowIndex++);
     i2c_OLED_send_string(dev, lineBuffer);
@@ -468,7 +468,7 @@ static void showBatteryPage(void)
         int32_t amperage = getBatteryCurrent();
         // 123456789012345678901
         // Amp: DDD.D mAh: DDDDD
-        tfp_sprintf(lineBuffer, "Amp: %d.%d mAh: %d", amperage / 100, (amperage % 100) / 10, getBatteryCapacityUsed());
+        tfp_sprintf(lineBuffer, "Amp: %ld.%ld mAh: %lu", amperage / 100, (amperage % 100) / 10, getBatteryCapacityUsed());
         padLineBuffer();
         i2c_OLED_set_line(dev, rowIndex++);
         i2c_OLED_send_string(dev, lineBuffer);
@@ -604,7 +604,7 @@ static void showBBPage(void)
 static void showDebugPage(void)
 {
     for (int rowIndex = 0; rowIndex < 4; rowIndex++) {
-        tfp_sprintf(lineBuffer, "%d = %5d", rowIndex, debug[rowIndex]);
+        tfp_sprintf(lineBuffer, "%d = %5ld", rowIndex, debug[rowIndex]);
         padLineBuffer();
         i2c_OLED_set_line(dev, rowIndex + PAGE_TITLE_LINE_COUNT);
         i2c_OLED_send_string(dev, lineBuffer);

src/main/osd/osd.c
@@ -677,7 +677,7 @@ static bool osdDisplayStat(int statistic, uint8_t displayRow)
 #ifdef USE_GPS
     case OSD_STAT_MAX_SPEED:
         if (featureIsEnabled(FEATURE_GPS)) {
-            tfp_sprintf(buff, "%d%c", osdGetSpeedToSelectedUnit(stats.max_speed), osdGetSpeedToSelectedUnitSymbol());
+            tfp_sprintf(buff, "%ld%c", osdGetSpeedToSelectedUnit(stats.max_speed), osdGetSpeedToSelectedUnitSymbol());
             osdDisplayStatisticLabel(displayRow, "MAX SPEED", buff);
             return true;
         }
@@ -736,7 +736,7 @@ static bool osdDisplayStat(int statistic, uint8_t displayRow)
 
     case OSD_STAT_USED_MAH:
         if (batteryConfig()->currentMeterSource != CURRENT_METER_NONE) {
-            tfp_sprintf(buff, "%d%c", getBatteryCapacityUsed(), SYM_MAH);
+            tfp_sprintf(buff, "%lu%c", getBatteryCapacityUsed(), SYM_MAH);
             osdDisplayStatisticLabel(displayRow, "USED MAH", buff);
             return true;
         }
@@ -834,9 +834,9 @@ static bool osdDisplayStat(int statistic, uint8_t displayRow)
         #define METERS_PER_KILOMETER 1000
         #define METERS_PER_MILE      1609
         if (osdConfig()->units == UNIT_IMPERIAL) {
-            tfp_sprintf(buff, "%d%c", statsConfig()->stats_total_dist_m / METERS_PER_MILE, SYM_MILES);
+            tfp_sprintf(buff, "%lu%c", statsConfig()->stats_total_dist_m / METERS_PER_MILE, SYM_MILES);
         } else {
-            tfp_sprintf(buff, "%d%c", statsConfig()->stats_total_dist_m / METERS_PER_KILOMETER, SYM_KM);
+            tfp_sprintf(buff, "%lu%c", statsConfig()->stats_total_dist_m / METERS_PER_KILOMETER, SYM_KM);
         }
         osdDisplayStatisticLabel(displayRow, "TOTAL DISTANCE", buff);
         return true;

src/main/osd/osd_elements.c
@@ -798,7 +798,7 @@ static void osdElementCurrentDraw(osdElementParms_t *element)
 
 static void osdElementDebug(osdElementParms_t *element)
 {
-    tfp_sprintf(element->buff, "DBG %5d %5d %5d %5d", debug[0], debug[1], debug[2], debug[3]);
+    tfp_sprintf(element->buff, "DBG %5ld %5ld %5ld %5ld", debug[0], debug[1], debug[2], debug[3]);
 }
 
 static void osdElementDisarmed(osdElementParms_t *element)
@@ -829,7 +829,7 @@ static void osdBackgroundDisplayName(osdElementParms_t *element)
 static void osdElementTotalFlights(osdElementParms_t *element)
 {
     const int32_t total_flights = statsConfig()->stats_total_flights;
-    tfp_sprintf(element->buff, "#%d", total_flights);
+    tfp_sprintf(element->buff, "#%ld", total_flights);
 }
 #endif
 
@@ -1011,7 +1011,7 @@ static void osdElementGpsSats(osdElementParms_t *element)
 static void osdElementGpsSpeed(osdElementParms_t *element)
 {
     if (STATE(GPS_FIX)) {
-        tfp_sprintf(element->buff, "%c%3d%c", SYM_SPEED, osdGetSpeedToSelectedUnit(gpsConfig()->gps_use_3d_speed ? gpsSol.speed3d : gpsSol.groundSpeed), osdGetSpeedToSelectedUnitSymbol());
+        tfp_sprintf(element->buff, "%c%3ld%c", SYM_SPEED, osdGetSpeedToSelectedUnit(gpsConfig()->gps_use_3d_speed ? gpsSol.speed3d : gpsSol.groundSpeed), osdGetSpeedToSelectedUnitSymbol());
     } else {
         tfp_sprintf(element->buff, "%c%c%c", SYM_SPEED, SYM_HYPHEN, osdGetSpeedToSelectedUnitSymbol());
     }
@@ -1096,7 +1096,7 @@ static void osdElementLogStatus(osdElementParms_t *element)
         } else {
             int32_t logNumber = blackboxGetLogNumber();
             if (logNumber >= 0) {
-                tfp_sprintf(element->buff, "%c%d", SYM_BBLOG, logNumber);
+                tfp_sprintf(element->buff, "%c%ld", SYM_BBLOG, logNumber);
             } else {
                 tfp_sprintf(element->buff, "%c", SYM_BBLOG);
             }
@@ -1107,7 +1107,7 @@ static void osdElementLogStatus(osdElementParms_t *element)
 
 static void osdElementMahDrawn(osdElementParms_t *element)
 {
-    tfp_sprintf(element->buff, "%4d%c", getBatteryCapacityUsed(), SYM_MAH);
+    tfp_sprintf(element->buff, "%4lu%c", getBatteryCapacityUsed(), SYM_MAH);
 }
 
 static void osdElementMainBatteryUsage(osdElementParms_t *element)
77090092dff069e423f2bf5c3ce3d3e7c27a0add
Petri Mattila <petri.t.j.mattila@gmail.com>
2026-04-24 09:22:57 +0100

    Cleanup in crsf.c

--- src/main/rx/crsf.c
+++ src/main/rx/crsf.c
@@ -529,6 +529,10 @@ STATIC_UNIT_TESTED uint8_t crsfFrameStatus(rxRuntimeState_t *rxRuntimeState)
             uint8_t startChannel = configByte & CRSF_SUBSET_RC_STARTING_CHANNEL_MASK;
             configByte >>= CRSF_SUBSET_RC_STARTING_CHANNEL_BITS;
 
+            if (startChannel >= CRSF_MAX_CHANNEL) {
+                return RX_FRAME_DROPPED;
+            }
+
             // get the channel resolution settings
             uint8_t channelBits;
             uint16_t channelMask;
@@ -563,10 +567,6 @@ STATIC_UNIT_TESTED uint8_t crsfFrameStatus(rxRuntimeState_t *rxRuntimeState)
 
             // calculate the number of channels packed
             uint8_t numOfChannels = ((crsfChannelDataFrame.frame.frameLength - CRSF_FRAME_LENGTH_TYPE_CRC - 1) * 8) / channelBits;
-            if (startChannel >= CRSF_MAX_CHANNEL) {
-                return RX_FRAME_DROPPED;
-            }
-
             numOfChannels = MIN(numOfChannels, CRSF_MAX_CHANNEL - startChannel);
 
             // unpack the channel data
